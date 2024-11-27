1a86bc43c71ce1c7b64634191c2f114325e00b27
Joachim Strömbergson <joachim@assured.se>
2024-11-27 13:46:52 +0100

    (rtl) Integrated the permutation module in the core module.
        Instantiate the ascon permutation module in the core
        module. Remove the permutation functionality from the core module
        and clean up the code.
    
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- src/rtl/ascon_core.v
+++ src/rtl/ascon_core.v
@@ -64,158 +64,50 @@ module ascon_core(
   localparam CTRL_NEXT     = 3'h2;
   localparam CTRL_FINALIZE = 3'h3;
   localparam CTRL_DONE     = 3'h4;
-
-
-  //----------------------------------------------------------------
-  // functions.
-  //----------------------------------------------------------------
-  // Constant addition layer (pc) for state word S2.
-  function automatic [63 : 0] pc(input [63 : 0] x, input [3 : 0] r);
-    begin : pc
-      reg [7 : 0] xor_val;
-      case (r)
-	    0:  xor_val = x[7 : 0] ^ 8'h3c;
-	    1:  xor_val = x[7 : 0] ^ 8'h2d;
-	    2:  xor_val = x[7 : 0] ^ 8'h1d;
-	    3:  xor_val = x[7 : 0] ^ 8'h0f;
-	    4:  xor_val = x[7 : 0] ^ 8'hf0;
-	    5:  xor_val = x[7 : 0] ^ 8'he1;
-	    6:  xor_val = x[7 : 0] ^ 8'hd2;
-	    7:  xor_val = x[7 : 0] ^ 8'hc3;
-	    8:  xor_val = x[7 : 0] ^ 8'hb4;
-	    9:  xor_val = x[7 : 0] ^ 8'ha5;
-	    10: xor_val = x[7 : 0] ^ 8'h96;
-	    11: xor_val = x[7 : 0] ^ 8'h87;
-	    12: xor_val = x[7 : 0] ^ 8'h78;
-	    13: xor_val = x[7 : 0] ^ 8'h69;
-	    14: xor_val = x[7 : 0] ^ 8'h5a;
-	    15: xor_val = x[7 : 0] ^ 8'h4b;
-	    default begin
-	    end
-      endcase
-      pc = {x[63 : 8], x[7 : 0] ^  xor_val};
-    end
-  endfunction // pc
-
-
-  // Linear diffusion (lp) for each state word.
-  function automatic [63 : 0] pls0(input [63 : 0] s0);
-    begin
-      pls0 = s0 ^ {s0[18 : 0], s0[63 : 19]} ^ {s0[27 : 0], s0[63 : 28]};
-    end
-  endfunction // pls0
-
-  function automatic [63 : 0] pls1(input [63 : 0] s1);
-    begin
-      pls1 = s1 ^ {s1[60 : 0], s1[63 : 61]} ^ {s1[38 : 0], s1[63 : 39]};
-    end
-  endfunction // pls1
-
-  function automatic [63 : 0] pls2(input [63 : 0] s2);
-    begin
-      pls2 = s2 ^ {s2[0], s2[63 : 1]} ^ {s2[5 : 0], s2[63 : 6]};
-    end
-  endfunction // pls2
-
-  function automatic [63 : 0] pls3(input [63 : 0] s3);
-    begin
-      pls3 = s3 ^ {s3[9 : 0], s3[63 : 10]} ^ {s3[16 : 0], s3[63 : 17]};
-    end
-  endfunction // pls3
-
-  function automatic [63 : 0] pls4(input [63 : 0] s4);
-    begin
-      pls4 = s4 ^ {s4[6 : 0], s4[63 : 7]} ^ {s4[40 : 0], s4[63 : 41]};
-    end
-  endfunction // pls4
-
-  function automatic [4 : 0] ps(input [4 : 0] x);
-    begin
-      case (x)
-        00: ps = 5'h04;
-        01: ps = 5'h0b;
-        02: ps = 5'h1f;
-        03: ps = 5'h14;
-        04: ps = 5'h1a;
-        05: ps = 5'h15;
-        06: ps = 5'h09;
-        07: ps = 5'h02;
-        08: ps = 5'h1b;
-        09: ps = 5'h05;
-        
-        10: ps = 5'h08;
-        11: ps = 5'h12;
-        12: ps = 5'h1d;
-        13: ps = 5'h03;
-        14: ps = 5'h06;
-        15: ps = 5'h1c;
-        16: ps = 5'h1e;
-        17: ps = 5'h13;
-        18: ps = 5'h07;
-        19: ps = 5'h0e;
-        
-        20: ps = 5'h00;
-        21: ps = 5'h0d;
-        22: ps = 5'h11;
-        23: ps = 5'h18;
-        24: ps = 5'h10;
-        25: ps = 5'h0c;
-        26: ps = 5'h01;
-        27: ps = 5'h19;
-        28: ps = 5'h16;
-        29: ps = 5'h0a;
-        
-        30: ps = 5'h0f;
-        01: ps = 5'h17;
-	    default begin
-	    end
-      endcase      
-    end
-  endfunction // ps
  
     
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg [63 : 0]  s0_reg;
-  reg [63 : 0]  s0_new;
-  reg [63 : 0]  s1_reg;
-  reg [63 : 0]  s1_new;
-  reg [63 : 0]  s2_reg;
-  reg [63 : 0]  s2_new;
-  reg [63 : 0]  s3_reg;
-  reg [63 : 0]  s3_new;
-  reg [63 : 0]  s4_reg;
-  reg [63 : 0]  s4_new;
-  reg           s_we;
-
   reg           ready_reg;
   reg           ready_new;
   reg           ready_we;
-
-  reg [3 : 0]   round_ctr_reg;
-  reg [3 : 0]   round_ctr_new;
-  reg           round_ctr_rst;
-  reg           round_ctr_inc;
-  reg           round_ctr_we;
-
-  reg [2 : 0]   ascon_ctrl_reg;
-  reg [2 : 0]   ascon_ctrl_new;
-  reg           ascon_ctrl_we;
 
   reg [127 : 0] result_reg;
   reg [127 : 0] result_new;
   reg           result_we;
 
+  reg [2 : 0]   ascon_core_ctrl_reg;
+  reg [2 : 0]   ascon_core_ctrl_new;
+  reg           ascon_core_ctrl_we;
 
+  
   //----------------------------------------------------------------
   // Wires.
   //----------------------------------------------------------------
-  reg state_init;
-  reg state_next;
+  reg [319 : 0]  permutation_block;
+  reg [3 : 0]    permutation_num_rounds;
+  reg            permutation_start;
+  wire           permutation_ready;
+  wire [319 : 0] permutation_result;
 
 
   //----------------------------------------------------------------
+  // ascon_permutation
+  // Instantiation of the Ascon permutation implementation.
+  //----------------------------------------------------------------
+  ascon_permutation 
+    ascon_permutation_inst(
+                           .clk(clk),
+                           .reset_n(reset_n),
+                           .block(permutation_block),
+                           .num_rounds(permutation_num_rounds),
+                           .start(permutation_start),
+                           .ready(permutation_ready),
+                           .result(permutation_result)
+                          );
+  
+    //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
   assign ready  = ready_reg;
@@ -231,147 +123,64 @@ module ascon_core(
   //----------------------------------------------------------------
   always @ (posedge clk or negedge reset_n)begin: reg_update
     if (!reset_n) begin
-      s0_reg         <= 64'h0;
-      s1_reg         <= 64'h0;
-      s2_reg         <= 64'h0;
-      s3_reg         <= 64'h0;
-      s4_reg         <= 64'h0;
-      round_ctr_reg  <= 4'h0;
-      result_reg     <= 128'h0;
-      ascon_ctrl_reg <= CTRL_IDLE;
+      ready_reg           <= 1'h0;
+      result_reg          <= 128'h0;
+      ascon_core_ctrl_reg <= CTRL_IDLE;
     end
 
     else begin
-      if (s_we) begin
-        s0_reg <= s0_new;
-        s1_reg <= s1_new;
-        s2_reg <= s2_new;
-        s3_reg <= s3_new;
-        s4_reg <= s4_new;
-      end
-
-      if (round_ctr_we) begin
-	    round_ctr_reg <= round_ctr_new;
+      if (ready_we) begin
+	    ready_reg <= ready_new;
       end
 
       if (result_we) begin
 	    result_reg <= result_new;
       end
       
-      if (ascon_ctrl_we) begin
-        ascon_ctrl_reg <= ascon_ctrl_new;
+      if (ascon_core_ctrl_we) begin
+        ascon_core_ctrl_reg <= ascon_core_ctrl_new;
       end
     end
   end // reg_update
 
 
   //----------------------------------------------------------------
-  // state_logic
-  // The actual logic to initialize and update the state.
-  // The state logic is a composition of PL, PS and PC
-  //----------------------------------------------------------------
-  always @*
-    begin : round_logic
-      integer i;
-      reg [4 : 0] ps_in;
-      reg [4 : 0] ps_out;
-      
-      reg [63 : 0] s0_pc;
-      reg [63 : 0] s0_ps;
-      reg [63 : 0] s1_ps;
-      reg [63 : 0] s2_ps;
-      reg [63 : 0] s3_ps;
-      reg [63 : 0] s4_ps;
-      
-      // Constant addition
-      s0_pc = pc(s0_reg, round_ctr_reg);
-
-      // Substitution
-      for (i = 0 ; i < 64 ; i = i + 1) begin
-        ps_in = {s4_reg[i], s3_reg[i], s2_reg[i], s1_reg[i], s0_pc[i]}; 
-
-        ps_out = ps(ps_in);
-
-        s0_ps[i] = ps_out[0];
-        s1_ps[i] = ps_out[1];
-        s2_ps[i] = ps_out[2];
-        s3_ps[i] = ps_out[3];
-        s4_ps[i] = ps_out[4];
-      end
-
-      // Linear diffusion
-      s0_new = pls0(s0_ps);
-      s1_new = pls1(s1_ps);
-      s2_new = pls2(s2_ps);
-      s3_new = pls3(s3_ps);
-      s4_new = pls4(s4_ps);
-    end // round_logic  
-  
-
-  //----------------------------------------------------------------
-  // round_ctr_logic
-  //----------------------------------------------------------------
-  always @*
-    begin : round_ctr
-      round_ctr_new = 4'h0;
-      round_ctr_we  = 1'h0;
-
-      if (round_ctr_rst) begin
-	    round_ctr_we = 1'h1;
-      end
-
-      if (round_ctr_inc) begin
-	    round_ctr_new = round_ctr_reg + 1'h1;
-	    round_ctr_we  = 1'h1;
-      end
-    end
-
-
-  //----------------------------------------------------------------
-  // ascon_ctrl
+  // ascon_core_ctrl
   //
-  // Control FSM for aes core.
+  // Control FSM for the Ascon core.
   //----------------------------------------------------------------
   always @*
-    begin : ascon_ctrl
-      s_we           = 1'h0;
-      result_we      = 1'h0;
-      ready_new      = 1'h0;
-      ready_we       = 1'h0;
-      state_init     = 1'h0;
-      state_next     = 1'h0;
-      round_ctr_rst  = 1'h0;
-      round_ctr_inc  = 1'h0;
-      ascon_ctrl_new = CTRL_IDLE;
-      ascon_ctrl_we  = 1'h0;
-
-      result_new = block ^ key;
+    begin : ascon_core_ctrl
+      result_we              = 1'h0;
+      ready_new              = 1'h0;
+      ready_we               = 1'h0;
+      ascon_core_ctrl_new    = CTRL_IDLE;
+      ascon_core_ctrl_we     = 1'h0;
+      permutation_block      = 32'hf;
+      permutation_num_rounds = 4'h7;
+      permutation_start      = 1'h0;
       
-      case (ascon_ctrl_reg)
+      case (ascon_core_ctrl_reg)
         CTRL_IDLE: begin
           if (init) begin
-	        result_we     = 1'h1;
 	        ready_new     = 1'h0;
 	        ready_we      = 1'h1;
-            ascon_ctrl_new = CTRL_INIT;
-            ascon_ctrl_we  = 1'h1;
+            ascon_core_ctrl_new = CTRL_INIT;
+            ascon_core_ctrl_we  = 1'h1;
           end
 
           if (next) begin
-            s_we          = 1'h1;
-	        result_we     = 1'h1;
 	        ready_new     = 1'h0;
 	        ready_we      = 1'h1;
-            ascon_ctrl_new = CTRL_NEXT;
-            ascon_ctrl_we  = 1'h1;
+            ascon_core_ctrl_new = CTRL_NEXT;
+            ascon_core_ctrl_we  = 1'h1;
           end
 
           if (finalize) begin
-	        result_we     = 1'h1;
 	        ready_new     = 1'h0;
 	        ready_we      = 1'h1;
-            ascon_ctrl_new = CTRL_FINALIZE;
-            ascon_ctrl_we  = 1'h1;
+            ascon_core_ctrl_new = CTRL_FINALIZE;
+            ascon_core_ctrl_we  = 1'h1;
           end
         end
 
@@ -379,35 +188,36 @@ module ascon_core(
         CTRL_INIT: begin
 	      ready_new     = 1'h1;
 	      ready_we      = 1'h1;
-          ascon_ctrl_new = CTRL_IDLE;
-          ascon_ctrl_we  = 1'h1;
+          ascon_core_ctrl_new = CTRL_IDLE;
+          ascon_core_ctrl_we  = 1'h1;
         end
 
         
         CTRL_NEXT: begin
 	      ready_new     = 1'h1;
 	      ready_we      = 1'h1;
-          ascon_ctrl_new = CTRL_IDLE;
-          ascon_ctrl_we  = 1'h1;
+          ascon_core_ctrl_new = CTRL_IDLE;
+          ascon_core_ctrl_we  = 1'h1;
         end
 
         
         CTRL_FINALIZE: begin
-          ascon_ctrl_new = CTRL_DONE;
-          ascon_ctrl_we  = 1'h1;
+          ascon_core_ctrl_new = CTRL_DONE;
+          ascon_core_ctrl_we  = 1'h1;
         end
         
   
         CTRL_DONE: begin
           ready_new     = 1'h1;
           ready_we      = 1'h1;
-          ascon_ctrl_new = CTRL_IDLE;
-          ascon_ctrl_we  = 1'h1;
+          ascon_core_ctrl_new = CTRL_IDLE;
+          ascon_core_ctrl_we  = 1'h1;
         end
 
         default: begin end
-      endcase // case (ascon_ctrl_reg)
-    end // ascon_ctrl
+      endcase // case (ascon_core_ctrl_reg)
+      
+    end // ascon_core_ctrl
 
 endmodule // ascon_core
 
